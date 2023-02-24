73c79c3fe3c3e4623bb1ae9f601447fdaf29c501
Piyush Srivastava <67270567+piyush-sri@users.noreply.github.com>
2023-02-24 11:24:26 +0530

    print Statement in python

---
+++ Python print statement/Hellopiyush.py
@@ -0,0 +1,20 @@
+#print statement is use to print output on the python shell.
+#two consecutive print statement will print output in two different lines
+#this is because the print statement by default ends with endline "\n"
+#For example print("Hello")=====print("Hello",end="\n")
+#thats why print statement show output in newline.
+print("Hello piyush founder of OneLinerNotes")
+
+
+#output: Hello piyush founder of OneLinerNotes
+
+print("Please Give a Star to this  Repository")
+
+#now if we execute or run these two python statement we will be getting the result in two sepereate line.
+#Output: 
+#Hello piyush founder of OneLinerNotes
+#Please Give a Star to this Repository
+
+#Also python is case sensitive that is print() and Print() are two different statement
+#print("Fork this repository")--- it works fine by giving the output as Fork this repository
+#Print("Fork this repository")   ----it will give you NameError in Python
